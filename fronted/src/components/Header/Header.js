ff26be8a60d04dcb2622bebff7cd739201427bab
BAO Qingjun <aibaoqj@163.com>
2024-08-31 02:10:47 -0400

    upload

---
+++ fronted/src/components/Header/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useCart } from '../../hooks/useCart';
+import classes from './header.module.css';
+
+export default function Header() {
+    const user = {
+        name: 'John',
+    };
+
+    const { cart } = useCart();
+
+    const logout = () => {
+
+    }
+
+    return (<header className={classes.header}>
+        <div className={classes.container}>
+            <Link to="/" className={classes.logo}>
+                Food Mine!
+            </Link>
+            <nav>
+                <ul>
+                    {
+                        user ? (
+                            <li className={classes.menu_container}>
+                                <Link to="/dashboard">{user.name}</Link>
+                                <div className={classes.menu}>
+                                    <Link to="/profile">Profile</Link>
+                                    <Link to="/Orders">Orders</Link>
+                                    <a onClick={logout}>Logout</a>
+                                </div>
+                            </li>
+                        ) : (
+                            <Link to="/login">Login</Link>
+                        )}
+
+                    <li>
+                        <Link to="/cart">
+                            Cart
+                            {cart.totalCount > 0 && (
+                                <span className={classes.cart_count}> {cart.totalCount} </span>
+                            )}
+                        </Link>
+                    </li>
+
+                </ul>
+            </nav>
+
+        </div>
+
+    </header>);
+}
